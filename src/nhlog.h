b5cf61ca1c22bc8bbb986ddd48169a8c33bbf901
Prashant <prashantrahul141@protonmail.com>
2024-08-20 05:43:51 +0530

    rename public apis

--- src/nhlog.h
+++ src/nhlog.h
@@ -14,23 +14,23 @@ typedef struct {
 } LogEvent;
 
 enum LogLevel {
-  LOG_TRACE,
-  LOG_DEBUG,
-  LOG_INFO,
-  LOG_WARN,
-  LOG_ERROR,
-  LOG_FATAL
+  NHLOG_TRACE,
+  NHLOG_DEBUG,
+  NHLOG_INFO,
+  NHLOG_WARN,
+  NHLOG_ERROR,
+  NHLOG_FATAL
 };
 
 void nhlog_set_level(int level);
 
 void nhlog_log(int level, const char *file, int line, const char *fmt, ...);
 
-#define log_trace(...) nhlog_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define log_debug(...) nhlog_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define log_info(...) nhlog_log(LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
-#define log_warn(...) nhlog_log(LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
-#define log_error(...) nhlog_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define log_fatal(...) nhlog_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+#define nhlog_trace(...) nhlog_log(NHLOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define nhlog_debug(...) nhlog_log(NHLOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define nhlog_info(...) nhlog_log(NHLOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define nhlog_warn(...) nhlog_log(NHLOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
+#define nhlog_error(...) nhlog_log(NHLOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define nhlog_fatal(...) nhlog_log(NHLOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
 #endif
