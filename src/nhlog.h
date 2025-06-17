e26a4f3c6900a73d6de0a8845906c814b0650b78
prashantrahul141 <prashantrahul141@protonmail.com>
2025-06-17 16:55:08 +0530

    feat: add logging to a outstream, more docs
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/nhlog.h
+++ src/nhlog.h
@@ -1,31 +1,67 @@
 #ifndef _NHLOG_H_
 #define _NHLOG_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 #include <stdarg.h>
 
+/*
+ * Internal representation of a log event.
+ */
 typedef struct {
+  // additional parameters
   va_list ap;
+  // format string
   const char *fmt;
+  // file in which it originated
   const char *file;
+  // time of the event
   struct tm *time;
+  // the stream to write to
   void *udata;
+  // at which line number it originated
   int line;
+  // level of the event
   int level;
 } LogEvent;
 
-enum LogLevel {
-  NHLOG_TRACE,
+/*
+ * Different log levels
+ */
+typedef enum {
+  NHLOG_TRACE = 0,
   NHLOG_DEBUG,
   NHLOG_INFO,
   NHLOG_WARN,
   NHLOG_ERROR,
   NHLOG_FATAL,
   NHLOG_OFF
-};
+} LogLevel;
 
-void nhlog_set_level(int level);
+/*
+ * Initializes the logger, should be called ATLEAST ONCE from anywhere before
+ * start logging.
+ * @param level - logging level
+ * @param outstream - where to write to, stderr will be used if given NULL.
+ */
+void nhlog_init(LogLevel level, void *outstream);
 
-void nhlog_log(int level, const char *file, int line, const char *fmt, ...);
+/*
+ * Sets the logging level
+ * @param level - the level
+ */
+void nhlog_set_level(LogLevel level);
+
+/*
+ * Sets the logging out stream
+ * @param fd - pointer to file stream, stderr will be used if given NULL.
+ */
+void nhlog_set_outstream(void *fd);
+
+void nhlog_log(LogLevel level, const char *file, int line, const char *fmt,
+               ...);
 
 #define nhlog_trace(...) nhlog_log(NHLOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define nhlog_debug(...) nhlog_log(NHLOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
@@ -33,5 +69,9 @@ void nhlog_log(int level, const char *file, int line, const char *fmt, ...);
 #define nhlog_warn(...) nhlog_log(NHLOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
 #define nhlog_error(...) nhlog_log(NHLOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define nhlog_fatal(...) nhlog_log(NHLOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif
