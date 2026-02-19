d91f5939c4cddda56db72145eeccc758de90e125
Prashant Rahul <prashantrahul141@protonmail.com>
2026-02-20 00:13:58 +0530

    feat: revamp implementation with zero initilisation
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- src/nhlog.c
+++ src/nhlog.c
@@ -14,74 +14,90 @@ static const char *level_colors[] = {"\x1b[94m", "\x1b[36m", "\x1b[32m",
 #include <stdio.h>
 #include <time.h>
 
+/*
+ * Global internal state of the logger
+ */
 static struct {
-  // current logging level
-  int level;
-  // file stream to write to
+  /* current logging level */
+  LogLevel level;
+  /* file stream to write to */
   FILE *fd;
-} logger_state;
+  /* whether to output colors or not */
+  bool output_colors;
+  /* whether to flush output stream after every log event */
+  bool immediate;
+} logger_state = {
+    .level = NHLOG_TRACE, .fd = NULL, .output_colors = true, .immediate = true};
 
-static void nhlog_stdout(LogEvent *event) {
+/*
+ * This writes a log event.
+ */
+static void nhlog_write(LogEvent *event) {
   char time_buffer[16];
   time_buffer[strftime(time_buffer, sizeof(time_buffer), "%H:%M:%S",
                        event->time)] = '\0';
 
-  bool is_file = !(stdout == event->udata || stderr == event->udata);
-  // if its not a file output with colors
-  if (!is_file) {
-    fprintf(event->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m %s",
+  /* if output_colors is set, output with colors */
+  if (logger_state.output_colors) {
+    fprintf(event->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%zu:\x1b[0m %s",
             time_buffer, level_colors[event->level],
             level_strings[event->level], event->file, event->line,
             level_colors[event->level]);
   } else {
-    fprintf(event->udata, "%s %-5s %s:%d: ", time_buffer,
+    fprintf(event->udata, "%s %-5s %s:%zu: ", time_buffer,
             level_strings[event->level], event->file, event->line);
   }
 
+  /* log message */
   vfprintf(event->udata, event->fmt, event->ap);
 
-  if (!is_file) {
-    // if its not a file output with colors
+  /* if output_colors is set, output with colors */
+  if (logger_state.output_colors) {
     fprintf(event->udata, "\n\x1b[0m");
   } else {
     fprintf(event->udata, "\n");
   }
 
-  fflush(event->udata);
+  /* flush output stream if immediate flag is set  */
+  if (logger_state.immediate) {
+    fflush(event->udata);
+  }
 }
 
-void nhlog_init(LogLevel level, FILE *outstream) {
-  logger_state.level = level;
-  logger_state.fd = NULL == outstream ? stderr : outstream;
-}
-
+/* Getter and setter for level */
 void nhlog_set_level(LogLevel level) { logger_state.level = level; }
-
 LogLevel nhlog_get_level(void) { return logger_state.level; }
 
+/* Getter and setter for output stream */
 void nhlog_set_outstream(FILE *fd) {
   logger_state.fd = NULL == fd ? stderr : fd;
 }
+FILE *nhlog_get_outstream(void) { return logger_state.fd; }
 
-void nhlog_log(LogLevel level, const char *file, int line, const char *fmt,
+/* Getter and setter for output colors */
+void nhlog_set_output_colors(bool oc) { logger_state.output_colors = oc; }
+bool nhlog_get_output_colors(void) { return logger_state.output_colors; }
+
+/* Getter and setter for immediate */
+void nhlog_set_immediate(bool i) { logger_state.immediate = i; }
+bool nhlog_get_immediate(void) { return logger_state.immediate; }
+
+void nhlog_log(LogLevel level, const char *file, size_t line, const char *fmt,
                ...) {
   time_t t = time(NULL);
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
   LogEvent event = {
+      .ap = {0},
       .fmt = fmt,
       .file = file,
       .time = localtime(&t),
-      .udata = logger_state.fd,
+      .udata = logger_state.fd == NULL ? stderr : logger_state.fd,
       .line = line,
       .level = level,
   };
-#pragma GCC diagnostic pop
 
   if (level >= logger_state.level) {
     va_start(event.ap, fmt);
-    nhlog_stdout(&event);
+    nhlog_write(&event);
     va_end(event.ap);
   }
 }
--- src/nhlog.h
+++ src/nhlog.h
@@ -6,7 +6,22 @@ extern "C" {
 #endif // __cplusplus
 
 #include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+
+/*
+ * Different log levels
+ */
+typedef enum : uint8_t {
+  NHLOG_TRACE = 0,
+  NHLOG_DEBUG,
+  NHLOG_INFO,
+  NHLOG_WARN,
+  NHLOG_ERROR,
+  NHLOG_FATAL,
+  NHLOG_OFF = 255
+} LogLevel;
 
 /*
  * Internal representation of a log event.
@@ -23,34 +38,14 @@ typedef struct {
   // the stream to write to
   FILE *udata;
   // at which line number it originated
-  int line;
+  size_t line;
   // level of the event
-  int level;
+  LogLevel level;
 } LogEvent;
 
 /*
- * Different log levels
- */
-typedef enum {
-  NHLOG_TRACE = 0,
-  NHLOG_DEBUG,
-  NHLOG_INFO,
-  NHLOG_WARN,
-  NHLOG_ERROR,
-  NHLOG_FATAL,
-  NHLOG_OFF
-} LogLevel;
-
-/*
- * Initializes the logger, should be called ATLEAST ONCE from anywhere before
- * start logging.
- * @param level - logging level
- * @param outstream - where to write to, stderr will be used if given NULL.
- */
-void nhlog_init(LogLevel level, FILE *outstream);
-
-/*
  * Sets the logging level
+ * default is NHLOG_TRACE
  * @param level - the level
  */
 void nhlog_set_level(LogLevel level);
@@ -63,19 +58,52 @@ LogLevel nhlog_get_level(void);
 
 /*
  * Sets the logging out stream
+ * default is stderr
  * @param fd - pointer to file stream, stderr will be used if given NULL.
  */
 void nhlog_set_outstream(FILE *fd);
 
-void nhlog_log(LogLevel level, const char *file, int line, const char *fmt,
+/*
+ * Retrieve current logging output stream
+ * @returns Current logging output stream
+ */
+FILE *nhlog_get_outstream(void);
+
+/*
+ * Sets whether to output colors or not
+ * default is set to true
+ * @param output_colors Bool
+ */
+void nhlog_set_output_colors(bool output_colors);
+
+/*
+ * Retrieve current output colors boolean
+ * @returns Current out colors boolean
+ */
+bool nhlog_get_output_colors(void);
+
+/*
+ * Sets whether to flush output stream after every log or not
+ * default is set to true
+ * @param immediate boolean
+ */
+void nhlog_set_immediate(bool immediate);
+
+/*
+ * Retrieve immediate flag is set or not
+ * @returns Current state of immediate flag
+ */
+bool nhlog_get_immediate(void);
+
+void nhlog_log(LogLevel level, const char *file, size_t line, const char *fmt,
                ...);
 
-#define nhlog_trace(...) nhlog_log(NHLOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define nhlog_debug(...) nhlog_log(NHLOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define nhlog_info(...) nhlog_log(NHLOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
-#define nhlog_warn(...) nhlog_log(NHLOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
-#define nhlog_error(...) nhlog_log(NHLOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define nhlog_fatal(...) nhlog_log(NHLOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+#define TRACE(...) nhlog_log(NHLOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define DEBUG(...) nhlog_log(NHLOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define INFO(...) nhlog_log(NHLOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define WARN(...) nhlog_log(NHLOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
+#define ERROR(...) nhlog_log(NHLOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define FATAL(...) nhlog_log(NHLOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
 #ifdef __cplusplus
 }
